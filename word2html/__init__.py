280e0c7b8bcddb8b3631dc9df43db150770cc44b
Brad Montgomery <brad@bradmontgomery.net>
2021-08-04 11:37:16 -0500

    use argparse, cleanup, black-formatting

--- word2html/__init__.py
+++ word2html/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 VERSION = __version__
 
 from .main import convert_to_html
--- word2html/main.py
+++ word2html/main.py
@@ -4,8 +4,10 @@ Quick & dirt script to convert a Word Docx file to HTML.
 Requires pypandoc pytidylib  (see requirements.txt)
 
 """
+import argparse
 import os
 import sys
+
 try:
     import pypandoc
     from tidylib import tidy_document
@@ -15,37 +17,38 @@ except ImportError:
 
 def convert_to_html(filename):
     # Do the conversion with pandoc
-    output = pypandoc.convert(filename, 'html')
+    output = pypandoc.convert(filename, "html")
 
     # Clean up with tidy...
-    output, errors = tidy_document(output,  options={
-        'numeric-entities': 1,
-        'wrap': 80,
-    })
+    output, errors = tidy_document(
+        output,
+        options={
+            "numeric-entities": 1,
+            "wrap": 80,
+        },
+    )
     print(errors)
 
     # replace smart quotes.
-    output = output.replace(u"\u2018", '&lsquo;').replace(u"\u2019", '&rsquo;')
+    output = output.replace(u"\u2018", "&lsquo;").replace(u"\u2019", "&rsquo;")
     output = output.replace(u"\u201c", "&ldquo;").replace(u"\u201d", "&rdquo;")
 
     # write the output
     filename, ext = os.path.splitext(filename)
     filename = "{0}.html".format(filename)
-    with open(filename, 'w') as f:
+    with open(filename, "w") as f:
         # Python 2 "fix". If this isn't a string, encode it.
         if type(output) is not str:
-            output = output.encode('utf-8')
+            output = output.encode("utf-8")
         f.write(output)
 
     print("Done! Output written to: {}\n".format(filename))
 
 
-def main():
-    if len(sys.argv) == 2:
-        convert_to_html(sys.argv[1])
-    else:
-        print("\nUSAGE: word2html <filename>\n")
-
-
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Convert a Word document to an HTML document."
+    )
+    parser.add_argument("path", type=str, help="Path to your word document")
+    args = parser.parse_args()
+    convert_to_html(args.path)
